9b978d677dc7fdc7e0f3f2e69afd662c2cab5324
s0vunia <burindima254@yandex.ru>
2024-12-17 14:54:41 +0300

    fix error handling

--- backend/internal/api/solution/deployStatus.go
+++ backend/internal/api/solution/deployStatus.go
@@ -5,6 +5,7 @@ import (
 
 	"gitlab.crja72.ru/gospec/go16/easydeploy/backend/internal/logger"
 	desc "gitlab.crja72.ru/gospec/go16/easydeploy/backend/pkg/solution_v1"
+	"go.uber.org/zap"
 )
 
 func (i *Implementation) DeployStatus(ctx context.Context, req *desc.GetDeployStatusRequest) (*desc.GetDeployStatusResponse, error) {
@@ -12,5 +13,9 @@ func (i *Implementation) DeployStatus(ctx context.Context, req *desc.GetDeploySt
 
 	// TODO
 	err := i.solutionService.DeployStatus(ctx)
-	return &desc.GetDeployStatusResponse{}, err
+	if err != nil {
+		logger.Error("failed to deploy solution", zap.Error(err))
+		return nil, InternalError
+	}
+	return &desc.GetDeployStatusResponse{}, nil
 }
