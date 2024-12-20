f31436781fcb8f94d8c00ddbfa6805bf363fce2b
Peter Filon <petr.omlet@gmail.com>
2024-12-20 14:22:15 +0300

    changed p color

--- web_app/tailwind.config.js
+++ web_app/tailwind.config.js
@@ -6,6 +6,20 @@ module.exports = {
 	},
 	plugins: [require("@tailwindcss/typography"), require("daisyui")],
 	daisyui: {
-		themes: ["light", "dark"]
+		//themes: ["light", "dark"]
+		themes: [
+			{
+				light: {
+					...require("daisyui/src/theming/themes")["light"],
+					primary: "#2C31FF",
+				},
+			},
+			{
+				dark: {
+					...require("daisyui/src/theming/themes")["dark"],
+					primary: "#2C31FF",
+				},
+			},
+		]
 	}
 }
